60fa67dbd532432436a3b1a646b1991cdc69c580
Devinish <TheDevinish@gmail.com>
2014-03-10 16:32:11 -0400

    Update doony.js

--- doony.js
+++ doony.js
@@ -646,7 +646,7 @@
     }
     
     $(function () {
-        $("#top-panel").append("<div id='logoarea'><a id='logo' href='http://google.com'><div>click anywhere</div></a><a id='logo2' href='http://google.com'><div>click anywhere</div></a><div>");
+        $("#top-panel").append("<div id='logoarea'><a id='logo' href='http://tiny.cc/signshop'><div>click anywhere</div></a><a id='logo2' href='http://tiny.cc/Beastnode'><div>click anywhere</div></a><div>");
     });
 
     $("#l10n-footer").after("<span class='doony-theme'>Browsing Jenkins with " +
