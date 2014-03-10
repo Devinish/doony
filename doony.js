efdbe626c2226832f2fa82b062c4cecb8e6b2d67
Devinish <TheDevinish@gmail.com>
2014-03-10 17:02:08 -0400

    Update doony.js

--- doony.js
+++ doony.js
@@ -325,7 +325,7 @@
     var colors = [
         '#C02942', // a red
         '#4ecdc4', // a bright green blue
-        '#6dc9ff', // signshop blue
+        '#ade1ff', // signshop blue
         '#556270', // a slate color
         '#542437', // purple
         '#8fbe00', // lime yellow
